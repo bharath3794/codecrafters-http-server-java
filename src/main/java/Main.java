72e09c681b59ebaa314d0809d8d277e1494e04d8
Venkat Talluri <bharath3794@gmail.com>
2024-07-03 03:38:50 -0500

    read header

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Main {
@@ -22,11 +24,25 @@ public class Main {
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        try (var reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+           // Read Request Line
            String[] requestLineArr = reader.readLine().split(" ");
+
+           // Read Headers
+           Map<String, String> headers = new HashMap<>();
+           String line = null;
+           while (!(line = reader.readLine()).isEmpty()) {
+               String[] keyValuePair = line.split(": ");
+               if (keyValuePair.length == 2)
+                   headers.put(keyValuePair[0], keyValuePair[1]);
+           }
+           // Request Response
            if (requestLineArr.length != 0 && Objects.equals(requestLineArr[1], "/")) {
                clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
            } else if (requestLineArr.length != 0 && requestLineArr[1] != null && requestLineArr[1].startsWith("/echo/")) {
                String responseBody = requestLineArr[1].substring(6);
+               clientSocket.getOutputStream().write(String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", responseBody.length(), responseBody).getBytes());
+           } else if (requestLineArr.length != 0 && requestLineArr[1] != null && requestLineArr[1].startsWith("/user-agent")) {
+               String responseBody = headers.getOrDefault("User-Agent", "");
                clientSocket.getOutputStream().write(String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", responseBody.length(), responseBody).getBytes());
            } else {
                clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
